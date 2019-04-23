6cdc2adc20515184be0053993da019d034717991
axixnix <henryirons@yahoo.com>
2019-04-23 22:10:01 +0100

    still tweaking

--- cart.php
+++ cart.php
@@ -77,7 +77,7 @@ function pre_r($array){
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Computer Science Bookshop</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" media="screen" href="cart.css">
