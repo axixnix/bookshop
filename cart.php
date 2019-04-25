5cd9f8a996aa457fe1cd8e6713c636ec2e1227c5
axixnix <henryirons@yahoo.com>
2019-04-25 22:30:57 +0100

    still tweaking

--- cart.php
+++ cart.php
@@ -93,7 +93,7 @@ $query ='SELECT * FROM books ORDER by id ASC';
 $result = mysqli_query($connect,$query);
 
 if($result):
-    if(mysqli_num_rows($result)>0):
+    if(mysqli_num_rows($result)>0)://the code under this line will only be executed if the database is not empty
         while($book = mysqli_fetch_assoc($result)):
            // print_r($book);
             ?>
